0f9aad369cebff9683b6eaf0ef32c81d8f1cdbc3
QinomeD <cpueater@interia.pl>
2023-05-29 20:22:58 +0200

    Cancelling the event is a good idea actually + config

--- src/main/java/qinomed/gentlepicking/GentlePicking.java
+++ src/main/java/qinomed/gentlepicking/GentlePicking.java
@@ -7,13 +7,17 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import qinomed.gentlepicking.config.GentlePickingCommonConfig;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("gentlepicking")
@@ -21,6 +25,7 @@ import net.minecraftforge.fml.common.Mod;
 public class GentlePicking {
 
     public GentlePicking() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, GentlePickingCommonConfig.SPEC, "gentlepicking-common.toml");
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -30,11 +35,14 @@ public class GentlePicking {
 
     @SubscribeEvent
     public static void useBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (GentlePickingCommonConfig.ONLY_HAND.get() && event.getItemStack() != ItemStack.EMPTY)
+            return;
         Level level = event.getWorld();
         BlockPos pos = event.getPos();
         Player player = event.getPlayer();
         BlockState state = level.getBlockState(pos);
         if ((level.mayInteract(player, pos) && !player.isSpectator() && state.is(PICKABLE) || (state.getBlock() instanceof FlowerBlock || state.getBlock() instanceof MushroomBlock || state.getBlock() instanceof FungusBlock)) && !state.is(BLACKLIST)) {
+            event.setCanceled(true);
             Block.dropResources(state, level, pos);
             level.playSound(player, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1, 1);
             level.removeBlock(pos, false);
